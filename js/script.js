fa4392fcbf30cb15174f95f063dd120380c4b1d5
Leonardo-Scotti <scottileonardo1@gmail.com>
2024-06-18 17:30:22 -0300

    Adição do JavaScript

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Constantes
+const fotoGrande = document.getElementById('fotoGrande')
+const desc = document.getElementById('desc')
+    // Função para Trocar a Foto
+function trocaImg(value) {
+    fotoGrande.src = "assets/" + value
+}
